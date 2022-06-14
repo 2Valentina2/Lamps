d84a9f8a4da270016e8522dbc0d1fa880095c1f1
2Valentina2 <91115831+2Valentina2@users.noreply.github.com>
2022-06-14 12:17:54 +0300

    Add files via upload
    Variant2

--- Lamps2/Lamps2/urls.py
+++ Lamps2/Lamps2/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from button.views import toggle
 
 urlpatterns = [
     path('', include('button.urls')),
     path('admin/', admin.site.urls),
+    path("toggle/", toggle),
 ]
